7b9d8133818956d180d5a2810771915e28cd6e12
AK-1121 <akmail255@gmail.com>
2014-11-28 12:42:45 +0400

    Functional test now checks we can input a to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -8,7 +8,7 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
+        found = resolve('/')#resolve described in Evernote
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
